5b02cba64359c41888e952b34c83865d1037aa09
Catrun404 <a.kerast@gmail.com>
2020-09-16 13:12:03 +0300

    defaultcoomit2

--- First/src/com/javarush/task/task05/task0529/Solution.java
+++ First/src/com/javarush/task/task05/task0529/Solution.java
@@ -1,5 +1,20 @@
 package com.javarush.task.task05.task0529;
 
-public class Solution {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        int sum = 0;
+        BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            String s = buffer.readLine();
+            if (s.equals("сумма")) {
+                break;
+            } else {
+                sum +=Integer.parseInt(s);
+            }
+        }
+        System.out.println(sum);
+    }
 }
